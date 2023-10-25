4e1517483c83afa7c8168cd35b2faf4ed40cb067
Anderson-Costa1993 <111035530+Anderson-Costa1993@users.noreply.github.com>
2023-10-25 00:11:50 -0300

    correçao navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,13 @@
 import style from "./navbar.module.css";
 import { useNavigate } from "react-router-dom";
-import {useState} from "react"
-import { UniqueCategory } from "../../routes/UniqueCategory";
+
 
 export function Navbar() {
   const navigate = useNavigate();
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleComponent = () => {
-    setIsOpen(!isOpen);
-  };
-
-
   return (
     <div className={style["caontainer-navBar"]}>
-      <i className="bi bi-list" onClick={() => toggleComponent()}></i>
+      <i className="bi bi-list"></i>
       <i className="bi bi-lightning-charge-fill" onClick={() => navigate("/")}></i>
       <i className="bi bi-handbag-fill" onClick={() => navigate("/cart")}></i>
     </div>
